b0c388e4e1209cd9eacb8650ca4cbb2129627d96
Jeremy Nelson <jermnelson@gmail.com>
2016-02-22 11:39:42 -0700

    Added more topics, afleshed out structure for presentation

--- web.py
+++ web.py
@@ -5,8 +5,19 @@ from flask import Flask, render_template, url_for
 
 course = Flask(__name__)
 
-@course.route("/Day <day>/<topic>")
-def show_topic(day, topic):
+@course.route("/help-downloads")
+def help_and_downloads():
+    return render_template("help-and-downloads.html")
+
+@course.route("/open-badge")
+def badge():
+    return render_template("open-badge.html")
+
+@course.route("/<day>/")
+@course.route("/<day>/<topic>")
+def show_topic(day, topic=None):
+    if topic is None:
+        return render_template("{}.html".format(day))
     return render_template("{}/index.html".format(topic))
 
 
